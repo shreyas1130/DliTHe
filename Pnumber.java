86fa836ec6ae52110f891fd474be474d8dcae466
shreyas1130 <99818883+shreyas1130@users.noreply.github.com>
2023-01-18 14:15:42 +0530

    Create Pnumber.java

---
+++ Pnumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+	public class star {
+
+	   public static void main(String[] args)
+
+	{
+	   int i,j,n;
+	  
+	   for(i=1;i<=4;i++)
+	   {
+		for(j=1;j<=i;j++)
+		  System.out.print(j);
+
+	    System.out.println("");
+	    }
+	}
+	}
